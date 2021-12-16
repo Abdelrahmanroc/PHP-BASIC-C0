e9ef2c18ab6c9751a565eada0d90fa31cc384001
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2021-12-16 14:24:19 +0100

    hi

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam = "Abdelrahman";
+    echo gettype($voornaam);
+    ?>
+
+<?php
+$leeftijd = 23;
+    echo gettype($leeftijd);
+    ?>
+
+<?php
+$leeftijd = 2.3;
+    echo gettype($leeftijd);
+    ?>
+
+
+<?php
+$test = true;
+    echo gettype($test);
+    ?>
+<?php
+$test = null;
+echo gettype($test);
+?>
+
+</body>
+</html> 
