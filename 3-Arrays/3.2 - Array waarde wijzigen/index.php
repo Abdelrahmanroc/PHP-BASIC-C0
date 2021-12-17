3b5de196ac92222c57acb9864e6e3ed17089096f
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2021-12-17 14:13:08 +0100

    3.2

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$myCatNames =  ['tray', 'GG', 'shira'];
+echo "<li>$myCatNames[0]</li><li>$myCatNames[1]</li><li>$myCatNames[2]</li>";
+?>
+
+</body>
+</html>
