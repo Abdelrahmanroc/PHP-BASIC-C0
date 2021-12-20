29e38d69622368e022c3cecc1b36abc90eedb58b
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2021-12-20 13:38:32 +0100

    4.1

---
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$getal1 = 112;
+$getal2 = 112;
+echo $getal1 + $getal2;
+echo "<br>";
+echo $getal1 - $getal2;
+echo "<br>";
+echo $getal1 * $getal2;
+echo "<br>";
+echo $getal1 / $getal2;
+
+?>
+</body>
+</html>
