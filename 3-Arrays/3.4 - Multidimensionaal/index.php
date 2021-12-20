3d93ec36d79064c10f93d10520cca6495f63318a
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2021-12-20 13:00:15 +0100

    yes

---
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$abcGetallen=array("A","B","C");
+
+$abcGetallen=array("24","56","78","90");
+
+print_r($abcGetallen);
+
+echo "A56","C90"
+?>
+</body>
+</html>
