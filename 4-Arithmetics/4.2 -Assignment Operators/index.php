02485c3790145656e75bebd0979946b24a907bb5
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2021-12-20 13:52:39 +0100

    4.2

---
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$getal1 = 112;
+echo $getal1+=112;
+echo "<br>";
+echo $getal1-=112;
+echo "<br>";
+echo $getal1*=112;
+echo "<br>";
+echo $getal1/=112;
+
+?>
+</body>
+</html>
