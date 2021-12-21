70877d6ddc4cbc69383899df7f95426e7074e9db
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2021-12-21 16:56:21 +0100

    5.6

---
+++ 5-Conditional_Statements/5.6 - Logical Operators/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$test1 = 15;
+$var1 = 5;
+
+if($var1 < 10 && $var1 > 12) {
+    echo "het getal zit tussen de 5 en de 15!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 16!";
+}
+
+?>
+</body>
+</html>
