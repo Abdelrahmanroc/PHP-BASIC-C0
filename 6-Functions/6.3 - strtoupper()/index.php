22f9d9e6fb7739c948f530e867d4c5d7245e7865
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2021-12-21 19:30:21 +0100

    6.3

---
+++ 6-Functions/6.3 - strtoupper()/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+function telAlleWoorden() {
+    $lengteZin="Mijn blauwe auto is een mooie snelle wagen";
+    echo strlen("$lengteZin");
+}
+
+
+
+telAlleWoorden();
+
+?>
+
+
+</body>
+</html>
