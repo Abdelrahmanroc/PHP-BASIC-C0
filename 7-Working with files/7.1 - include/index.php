ad47cd7d6ce517cc8fdab7d4a9bb067e053cb07f
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2021-12-22 17:44:21 +0100

    new

--- 7-Working with files/7.1 - include/index.php
+++ 7-Working with files/7.1 - include/index.php
@@ -1,17 +1,9 @@
 <html>
-<head>
-     <title></title>
-     <?php include 'includes/functions.php';?>
-</head>
+<head></head>
 <body>
 
 <div class="menu">
 <?php include 'menu.php';?>
 </div>
-
-<h1>Welcome to my home page!</h1>
-<p>Some text.</p>
-<p>Some more text.</p>
-
 </body>
 </html>
