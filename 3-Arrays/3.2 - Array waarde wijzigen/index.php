74fb8f4c87fa995b824c442f26a5597e8843678c
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2022-01-17 10:44:20 +0100

    new

--- 3-Arrays/3.2 - Array waarde wijzigen/index.php
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -3,9 +3,11 @@
 <body>
 
 <?php
-$myCatNames =  ['tray', 'GG', 'shira'];
-echo "<li>$myCatNames[0]</li><li>$myCatNames[1]</li><li>$myCatNames[2]</li>";
+$mydog = array ("geto", "meto", "veto");
+echo $mydog[0] .' '.  $mydog[1] .' '.  $mydog[2];
+echo "<br><hr>";
+$mydog[2] = 'Poep';
+echo $mydog[0] .' '.  $mydog[1] .' '.  $mydog[2];
 ?>
-
 </body>
-</html>
+</html> 
